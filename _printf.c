c7b597304b1f479cd8e270ca2e572beb14effc8e
s3doon7amid <138930687+s3doon7amid@users.noreply.github.com>
2023-09-25 20:19:02 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,6 +4,7 @@
  * _printf - implementation of the inbuilt printf
  * @format: the format specifier
  * Return: the formated string
+ * Return: the formated string
  */
 
 int _printf(const char *format, ...)
