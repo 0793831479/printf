cc7f2fdd077f1e52669fa34917ad3606b866c8ac
s3doon7amid <138930687+s3doon7amid@users.noreply.github.com>
2023-09-25 20:21:50 +0200

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -11,6 +11,7 @@
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed.
+ * Return
  */
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
